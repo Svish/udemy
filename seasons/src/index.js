d02cbd32b7d1b356b7bc1f29effc261cc856a57c
Torleif Berger <torleif@outlook.com>
2019-02-24 18:59:01 +0100

    Section 6

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    lat: null,
+    errorMessage: '',
+  };
 
-    this.state = {
-      lat: null,
-      errorMessage: '',
-    };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      e => {
-        console.warn(e);
-        this.setState({ errorMessage: e.message });
-      },
+      position => this.setState({ lat: position.coords.latitude }),
+      e => console.warn(e) & this.setState({ errorMessage: e.message }),
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Waiting for location..." />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
