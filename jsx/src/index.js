fb0501c5bd78507283f2a090b8f1698b92faf7b9
Torleif Berger <torleif@outlook.com>
2019-02-24 14:18:48 +0100

    Section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: 'Click me' };
+  const style = { backgroundColor: 'blue', color: 'white' };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  );
 };
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
