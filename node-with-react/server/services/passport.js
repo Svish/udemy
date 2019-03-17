40393f9cc8048635487b035947bdb03c2c3ce5f4
Torleif Berger <torleif@outlook.com>
2019-03-17 11:32:02 +0100

    Added proxy setting for GoogleStrategy

--- node-with-react/server/services/passport.js
+++ node-with-react/server/services/passport.js
@@ -20,6 +20,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: '/auth/google/callback',
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
