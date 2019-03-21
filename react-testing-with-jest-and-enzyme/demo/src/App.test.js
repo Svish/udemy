0b95c9f3c0334234bbbca165e3277b7d15e9442a
Torleif Berger <torleif@outlook.com>
2019-03-21 19:50:34 +0100

    React Testing: Section 1

--- react-testing-with-jest-and-enzyme/demo/src/App.test.js
+++ react-testing-with-jest-and-enzyme/demo/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
